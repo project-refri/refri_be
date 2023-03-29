afe90d4c7f73ce7a5d00243ba0c6428a7baa4788
KeisLuv5991 <chmhj9997@gmail.com>
2023-03-29 18:44:26 +0900

    add unit test suite

--- app/libs/image/src/image.service.spec.ts
+++ app/libs/image/src/image.service.spec.ts
@@ -1,18 +1,63 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { ImageService } from './image.service';
+import { TestBed } from '@automock/jest';
+import {
+  S3Client,
+  PutObjectCommand,
+  PutObjectCommandInput,
+} from '@aws-sdk/client-s3';
+import { ConfigService } from '@nestjs/config';
+import { ImageDomain } from './image-domain.enum';
+import { v4 as uuid4 } from 'uuid';
+
+jest.mock('uuid', () => ({
+  v4: jest.fn().mockReturnValue('test-uuid'),
+}));
 
 describe('ImageService', () => {
   let service: ImageService;
+  let s3Client: jest.Mocked<S3Client>;
+  let configService: jest.Mocked<ConfigService>;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ImageService],
-    }).compile();
+    const { unit, unitRef } = TestBed.create(ImageService).compile();
 
-    service = module.get<ImageService>(ImageService);
+    service = unit;
+    s3Client = unitRef.get(S3Client);
+    configService = unitRef.get(ConfigService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('uploadImage', () => {
+    it('should upload an image to S3 and return its URL', async () => {
+      const domain: ImageDomain = ImageDomain.DEFAULT;
+      const image: Express.Multer.File = {
+        buffer: Buffer.from('test image'),
+        mimetype: 'image/jpeg',
+        size: 100,
+        fieldname: '',
+        originalname: '',
+        encoding: '',
+        stream: '' as any,
+        destination: '',
+        filename: '',
+        path: '',
+      };
+      const params: PutObjectCommandInput = {
+        Bucket: 'test-bucket',
+        ACL: 'public-read',
+        Key: domain.toString() + '-' + uuid4() + '.jpg',
+        Body: image.buffer,
+      };
+      configService.get.mockReturnValue('test-bucket');
+
+      const result = await service.uploadImage({ domain, image });
+
+      expect(s3Client.send).toHaveBeenCalledWith(expect.any(PutObjectCommand));
+      expect(configService.get).toHaveBeenCalledWith(
+        'AWS_S3_IMAGE_MAIN_BUCKET',
+      );
+      expect(result).toEqual({
+        url: expect.stringContaining(ImageDomain.DEFAULT + '-'),
+      });
+    });
   });
 });
