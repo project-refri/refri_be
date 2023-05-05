4ad801f0df899ce27b23276c2b96b7a5dc589822
happy-juny <38745815+Istiopaxx@users.noreply.github.com>
2023-05-05 19:16:06 +0900

    [Feat]: migrate to gcp cloud run (#33)

--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
+# aws
+
 variable "aws_region" {
   description = "AWS region"
   type        = string
@@ -7,5 +9,41 @@ variable "aws_region" {
 variable "aws_s3_image_main_bucket_name" {
   description = "AWS S3 bucket name for image main"
   type        = string
-  default     = "refri-image-main"
+  sensitive   = true
 }
+
+# gcp
+
+variable "cloud_run_image_name" {
+  type      = string
+  sensitive = true
+}
+
+
+# api-server
+
+variable "DATABASE_URI" {
+  type      = string
+  sensitive = true
+}
+
+variable "JWT_SECRET" {
+  type      = string
+  sensitive = true
+}
+
+variable "JWT_EXPIRES_IN" {
+  type      = number
+  sensitive = true
+}
+
+variable "JWT_REFRESH_EXPIRES_IN" {
+  type      = number
+  sensitive = true
+}
+
+variable "AWS_S3_IMAGE_MAIN_BUCKET" {
+  type      = string
+  sensitive = true
+}
+
