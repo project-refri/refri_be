0244a21997433c7f801e8d48d998a3bc99774b7d
istiopaxx <chmhj9997@gmail.com>
2023-11-18 02:05:02 +0900

    fix pm2 cluster instance

--- api/ecosystem.config.js
+++ api/ecosystem.config.js
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 module.exports = [
   {
     name: 'app',
     script: 'main.js',
     exec_mode: 'cluster',
-    instances: 'max',
+    instances: Math.max(require('os').cpus().length - 1, 1), // CPU 코어 수에서 1를 뺀 값
   },
 ];
