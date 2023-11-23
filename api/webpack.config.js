7919d9e3c1cf366f4ae03484baaf3c058c97ceb0
istiopaxx <chmhj9997@gmail.com>
2023-11-24 01:15:10 +0900

    [Fix]: include prisma query engine to docker image

--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,4 +1,5 @@
 const TerserPlugin = require('terser-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (options, webpack) => {
   const lazyImports = [
@@ -36,6 +37,18 @@ module.exports = (options, webpack) => {
     },
     plugins: [
       ...options.plugins,
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: `./node_modules/.prisma/client/libquery*.node`,
+            to({ context, absoluteFilename }) {
+              return `./apps/api-bundled/.prisma/client/${absoluteFilename
+                .split('/')
+                .pop()}`;
+            },
+          },
+        ],
+      }),
       new webpack.IgnorePlugin({
         checkResource(resource) {
           if (lazyImports.includes(resource)) {
