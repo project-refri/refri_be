9f14a61d0858c2323ee4d71d5f08f5cc0a1dc0da
istiopaxx <chmhj9997@gmail.com>
2023-12-07 02:26:26 +0900

    improve type safety on util funcs

--- api/libs/common/src/utils/transfer-objectId.ts
+++ api/libs/common/src/utils/transfer-objectId.ts
@@ -1,10 +1,13 @@
 import { Types } from 'mongoose';
 
-export function transferObjectId<T>(obj: T, keys: string[]) {
+export function transferObjectId<T, K extends keyof T>(
+  obj: T,
+  keys: readonly K[],
+) {
   const ret: T = { ...obj };
   for (const key of keys) {
     if (obj[key]) {
-      ret[key] = new Types.ObjectId(obj[key]);
+      ret[key] = new Types.ObjectId(obj[key] as any) as any;
     }
   }
   return ret;
